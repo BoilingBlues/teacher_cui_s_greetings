fcee2b075c6bef33093169d8d8e9842fdca993e7
BoilingBlues <qq22430767@gmail.com>
2022-03-05 01:26:36 +0800

    BoilingBlues --commit) Implement windows client demo

---
+++ README.md
@@ -0,0 +1,16 @@
+# Teacher CUI's Greetings
+
+A teacher friend of mine insisted that I write a software for him. I don't want to say who it is.
+
+## DESCRIPTION
+
+This guy wants to use a remote device to send a message, and the message can be displayed as a floating window on a Windows OS computer on the classroom blackboard screen.
+
+### Windows Client
+
+Developed with electron.   
+Full-screen transparent window to show message, with a tray that controls display, hide and exit.
+
+
+******* 
+To Be Developed...
---
+++ client-win/main.js
@@ -0,0 +1,100 @@
+const { nativeImage, Tray, Menu } = require('electron');
+const path = require('path');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+var mainWindow = null;
+var tray = null;
+var contextMenu = null;
+
+initChromuim();
+
+app.on('ready', function () {
+    createMainWindow();
+    initTrayMenu();
+});
+
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        transparent: true,
+        frame: false,
+        resizable: false,
+        alwaysOnTop: true,
+        center: true,
+        skipTaskbar: true,
+        autoHideMenuBar: true,
+        focusable: false
+    });
+
+    mainWindow.setAlwaysOnTop(true, 'pop-up-menu');
+
+    mainWindow.maximize();
+    mainWindow.setIgnoreMouseEvents(true);
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+}
+function initChromuim() {
+    app.commandLine.appendSwitch('enable-gpu-rasterization', true);
+    app.commandLine.appendSwitch('enable-native-gpu-memory-buffers', true);
+    app.commandLine.appendSwitch('high-dpi-support', true);
+    app.commandLine.appendSwitch('device-scale-factor', true);
+    app.commandLine.appendSwitch('disable-touch-adjustment', true);
+    app.commandLine.appendSwitch('main-frame-resizes-are-orientation-changes', true);
+    app.commandLine.appendSwitch('disable-pinch', true);
+}
+
+function showMainWindow() {
+    if (mainWindow) {
+        mainWindow.show()
+    }
+}
+function hideMainWindow() {
+    mainWindow.hide();
+}
+
+function handleToggleShowMainWindow() {
+    if (mainWindow && mainWindow.isVisible()) {
+        hideMainWindow();
+    } else {
+        showMainWindow();
+    }
+}
+
+
+function initTrayMenu() {
+    try {
+        let iconPath = path.join(__dirname, "ico/favicon.png");
+        console.log(iconPath);
+        const nimage = nativeImage.createFromPath(iconPath);
+
+        tray = new Tray(nimage);
+        tray.setToolTip('teacher cui');
+        contextMenu = Menu.buildFromTemplate([
+            {
+                label: '显示',
+                type: 'normal',
+                click: showMainWindow
+            },
+            {
+                label: '隐藏',
+                type: 'normal',
+                click: hideMainWindow
+            },
+            {
+                label: '退出',
+                type: 'normal',
+                click: function () {
+                    app.quit();
+                }
+            }
+
+        ]);
+
+        tray.setContextMenu(contextMenu);
+        tray.on('click', handleToggleShowMainWindow);
+    } catch (error) {
+        console.log(error);
+    }
+}
